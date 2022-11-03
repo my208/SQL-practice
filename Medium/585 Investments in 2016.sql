b0d616a14075bc599a60da3e8a1f55b4ea019b79
Monica Yuan <ym21@Monicas-MacBook-Pro.local>
2022-11-02 22:12:50 -0700

    add 585 Investments in 2016.sql

--- Medium/585 Investments in 2016.sql
+++ Medium/585 Investments in 2016.sql
@@ -9,4 +9,4 @@ WHERE cnt_loc = 1 AND cnt_15 > 1;
 SELECT ROUND(SUM(tiv_2016), 2) tiv_2016
 FROM Insurance i1
 WHERE 1 = (SELECT COUNT(pid) FROM Insurance i2 WHERE i1.lat = i2.lat AND i1.lon = i2.lon)
-AND 1 < (SELECT COUNT(pid) FROM Insurance i3 WHERE i1.tiv_2015 = i3.tiv_2015)
+AND 1 < (SELECT COUNT(pid) FROM Insurance i3 WHERE i1.tiv_2015 = i3.tiv_2015);
